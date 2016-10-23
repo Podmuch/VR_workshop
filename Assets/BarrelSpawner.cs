7cdc6c383a79d4c3cd04d9a4336025fba9e079be
Dominik <dominikpielak@gmail.com>
2016-10-23 15:53:27 +0200

    VR workshops - Thursday changes

---
+++ Assets/BarrelSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelSpawner : MonoBehaviour
+{
+    public Transform BarrelPrefab;
+
+    public float SpawnInterval;
+
+    void Start()
+    {
+        StartCoroutine(SpawingCoroutine());
+    }
+
+    IEnumerator SpawingCoroutine()
+    {
+        while(true)
+        {
+            Transform newBarrel = Instantiate<Transform>(BarrelPrefab);
+            newBarrel.position = new Vector3(Random.Range(0, 25), 15, Random.Range(-10,15));
+            yield return new WaitForSeconds(SpawnInterval);
+        }
+    }
+}
